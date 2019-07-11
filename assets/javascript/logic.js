5c3e3ad9a4255fe51fab2499427739760a95777e
Aida Yrysbekova <aidiko20@gmail.com>
2019-07-10 23:56:22 -0400

    gifs are displayed but in random topics

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -4,23 +4,38 @@ $(document).ready(function () {
     var topics = ["Avengers", "Friends", "Harry Potter", "How I met your mother", "Beauty and the Beast", "Pride and Predjudice"]
 
     console.log(topics);
-    //
+    //looping on topics
     for (var i = 0; i < topics.length; i++) {
         var button = $("<button>").text(topics[i]);
         $("#display-buttons").append(button);
     }
+        $(document).click("button", function () {
+            var movie = $(this).attr("#data-movie")
+            var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=6FeZkmjXbl4mABjEJ76VZMnA3pOdhYNB&&limit=10";
+            $.ajax({
+                url: queryUrl,
+                method: "GET"
+            }).then(function (response) {
+                var results = response.data
+                for (var i = 0; i < topics.length; i++) {
 
+                    var showDiv = $("<div>");
+                    var p = $("<p>").text("Rating: " + results[i].rating);
+                    var showImage = $("<img>");
+ 
+                    showImage.attr("src", results[i].images.fixed_height.url);
 
-    $(document).click("button", function () {
-        var movie = $(this).attr("data-movie")
-        var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=6FeZkmjXbl4mABjEJ76VZMnA3pOdhYNB&q=" + movie + "&limit=10";
-        $.ajax({
-            url: queryUrl,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response)
+                    showDiv.append(p);
+                    showDiv.append(showImage);
+
+                    $("#display-images").prepend(showDiv);
+                }
+                console.log(response)
+            });
         });
     });
-});
+
+
+
 
 
