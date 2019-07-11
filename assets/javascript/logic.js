c3e6fb0fe7fe51b3f9266325fc80d79bf92c1b31
Aida Yrysbekova <aidiko20@gmail.com>
2019-07-10 22:30:56 -0400

    added query url

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,12 +1,26 @@
 $(document).ready(function () {
 
-
+    // My variables
     var topics = ["Avengers", "Friends", "Harry Potter", "How I met your mother", "Beauty and the Beast", "Pride and Predjudice"]
 
     console.log(topics);
+    //
     for (var i = 0; i < topics.length; i++) {
         var button = $("<button>").text(topics[i]);
         $("#display-buttons").append(button);
     }
 
+
+    $(document).click("button", function () {
+        var movie = $(this).attr("data-movie")
+        var queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=6FeZkmjXbl4mABjEJ76VZMnA3pOdhYNB&q=" + movie + "&limit=10";
+        $.ajax({
+            url: queryUrl,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
+        });
+    });
 });
+
+
